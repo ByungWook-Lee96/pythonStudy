60c1bf24f6939b1ba0250d8f0e7831035f2ca014
ByungWook-Lee96 <byungwook414@gmail.com>
2023-09-20 16:57:56 +0900

    반복문과 함수 추가

---
+++ exam/달리기 경주(stack).py
@@ -0,0 +1,14 @@
+
+players = ["mumu", "soe", "poe", "kai", "mine"]
+callings = ["kai", "kai", "mine", "mine"]
+
+answer = []
+
+
+# linked list 와 같이 사용 필요
+# dictionary 형식으로 players를 key에 넣고, value에는 key의 앞과 뒤에 선수를 넣음
+# calling이 될 때마다 4명의 선수 안에 value를 변경(지목된 선수, 지목된 선수의 앞사람, 지목된 선수의 뒷사람, 지목된 선수의 앞의 앞사람)
+# top을 만들어서 가장 앞에 있는 사람이 누구인지 따짐(1등은 앞 사람이 없는 사람을 찾으면 됨)
+
+
+
